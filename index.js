302f906841ac6c56289580b807d67a59eb7302e7
jackcornblum <jcornblum@gmail.com>
2021-05-25 10:24:49 -0400

    commit tree 27be904edcde3ba8bf125ca1eed4641801435a85

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+// };
+
+// fetch('http://localhost:3000/dogs', configurationObject);
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => document.body.append(data.id))
+    .catch(error => {
+        document.body.append(`Ruh roh raggy, ${error}`);
+        
+    })
+}
